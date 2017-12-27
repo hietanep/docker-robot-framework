c4b11b0178d215e0f1d5d9a3b8e11cb4f12c7e7b
Päivi Hietanen <paivi.hietanen@pohjantahti.fi>
2017-12-27 12:33:27 +0200

    Added argument passin to shell script

--- bin/run-tests-in-virtual-screen.sh
+++ bin/run-tests-in-virtual-screen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-xvfb-run --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac" robot --outputDir /opt/robotframework/reports --include ${TAG} /opt/robotframework/tests
+TAG=$1
+xvfb-run --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac" robot --outputDir /opt/robotframework/reports --include $TAG /opt/robotframework/tests
