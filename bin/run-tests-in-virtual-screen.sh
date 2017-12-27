f073de0d4822b0b167e98688f7f50e430c08ce64
Päivi Hietanen <paivi.hietanen@pohjantahti.fi>
2017-12-27 13:24:53 +0200

    Added argument checking to shell script

--- bin/run-tests-in-virtual-screen.sh
+++ bin/run-tests-in-virtual-screen.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+# first argument is used as TAG to define witch test are run
 TAG=$1
+
+if [ "$TAG" == ""]
+then
+xvfb-run --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac" robot --outputDir /opt/robotframework/reports /opt/robotframework/tests
+else
 xvfb-run --server-args="-screen 0 ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x${SCREEN_COLOUR_DEPTH} -ac" robot --outputDir /opt/robotframework/reports --include $TAG /opt/robotframework/tests
+fi
